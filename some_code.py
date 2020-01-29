2794c24c3690479da0cee870fc2d550695f7eb20
Vladka GG <vladlenborgardt@gmail.com>
2020-01-29 21:21:28 +0300

    train with ssh

--- some_code.py
+++ some_code.py
@@ -1,10 +1,13 @@
-class SomeClass:
+def generator(iteration: int):
     a = 0
+    for x in range(iteration):
+        a += 1
+        yield a
 
-    def go(self):
-        self.a += 1
-        yield 1
 
-
-obj = SomeClass()
-print(obj.go())
+gen = generator(5)
+try:
+    for i in range(6):
+        print(next(gen))
+except StopIteration:
+    print('It is end.')
